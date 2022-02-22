f2edcd472a9b1ebf40dd78f3d73c7b930d251867
Alex <alexandre_2420@outlook.com>
2022-02-22 16:08:47 -0500

    refactored: extract Form Component and Context Store

--- src/App.js
+++ src/App.js
@@ -1,81 +1,10 @@
-import React, { useContext, useReducer, useEffect, useRef, useState, createContext } from 'react';
+import React, { useContext,useEffect } from 'react'
+//context
+import StoreProvider, {Store} from './context/AppContext'
+//components
+import { Form } from './components/Form'
 
-const HOST_API = "http://localhost:8080/api";
-const initialState = {
-  todo: { list: [], item: {} }
-};
-const Store = createContext(initialState)
-
-
-const Form = () => {
-  const formRef = useRef(null);
-  const { dispatch, state: { todo } } = useContext(Store);
-  const item = todo.item;
-  const [state, setState] = useState(item);
-
-  const onAdd = (event) => {
-    event.preventDefault();
-
-    const request = {
-      name: state.name,
-      id: null,
-      completed: false
-    };
-
-
-    fetch(HOST_API + "/todo", {
-      method: "POST",
-      body: JSON.stringify(request),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(response => response.json())
-      .then((todo) => {
-        dispatch({ type: "add-item", item: todo });
-        setState({ name: "" });
-        formRef.current.reset();
-      });
-  }
-
-  const onEdit = (event) => {
-    event.preventDefault();
-
-    const request = {
-      name: state.name,
-      id: item.id,
-      isCompleted: item.isCompleted
-    };
-
-
-    fetch(HOST_API + "/todo", {
-      method: "PUT",
-      body: JSON.stringify(request),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(response => response.json())
-      .then((todo) => {
-        dispatch({ type: "update-item", item: todo });
-        setState({ name: "" });
-        formRef.current.reset();
-      });
-  }
-
-  return <form ref={formRef}>
-    <input
-      type="text"
-      name="name"
-      placeholder="¿Qué piensas hacer hoy?"
-      defaultValue={item.name}
-      onChange={(event) => {
-        setState({ ...state, name: event.target.value })
-      }}  ></input>
-    {item.id && <button onClick={onEdit}>Actualizar</button>}
-    {!item.id && <button onClick={onAdd}>Crear</button>}
-  </form>
-}
+const HOST_API = "http://localhost:8080/api"
 
 
 const List = () => {
@@ -150,53 +79,6 @@ const List = () => {
 }
 
 
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'update-item':
-      const todoUpItem = state.todo;
-      const listUpdateEdit = todoUpItem.list.map((item) => {
-        if (item.id === action.item.id) {
-          return action.item;
-        }
-        return item;
-      });
-      todoUpItem.list = listUpdateEdit;
-      todoUpItem.item = {};
-      return { ...state, todo: todoUpItem }
-    case 'delete-item':
-      const todoUpDelete = state.todo;
-      const listUpdate = todoUpDelete.list.filter((item) => {
-        return item.id !== action.id;
-      });
-      todoUpDelete.list = listUpdate;
-      return { ...state, todo: todoUpDelete }
-    case 'update-list':
-      const todoUpList = state.todo;
-      todoUpList.list = action.list;
-      return { ...state, todo: todoUpList }
-    case 'edit-item':
-      const todoUpEdit = state.todo;
-      todoUpEdit.item = action.item;
-      return { ...state, todo: todoUpEdit }
-    case 'add-item':
-      const todoUp = state.todo.list;
-      todoUp.push(action.item);
-      return { ...state, todo: {list: todoUp, item: {}} }
-    default:
-      return state;
-  }
-}
-
-const StoreProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  return <Store.Provider value={{ state, dispatch }}>
-    {children}
-  </Store.Provider>
-
-}
-
 function App() {
   return <StoreProvider>
     <h3>To-Do List</h3>
@@ -205,4 +87,4 @@ function App() {
   </StoreProvider>
 }
 
-export default App;
+export default App
---
+++ src/context/AppContext.js
@@ -0,0 +1,21 @@
+import { createContext, useReducer } from "react";
+//reducer
+import reducer from "../reducer"
+
+const initialState = {
+    todo: { list: [], item: {} }
+}
+
+export const Store = createContext(initialState)
+
+
+const StoreProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return <Store.Provider value={{ state, dispatch }}>
+      {children}
+    </Store.Provider>
+
+}
+
+export default StoreProvider
